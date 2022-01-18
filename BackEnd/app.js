aece0ce9f635116c3b0b363bfca1f23086a54922
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2022-01-19 01:33:34 +0530

    Replaced mongoclient with mongoose

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,51 +7,55 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 const publicdirectory = path.join(__dirname,'./public')
 app.use(express.static(publicdirectory));
-const url="mongodb+srv://cp-user:12345@cluster.ye5s9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const url="mongodb+srv://cp-user:12345@cluster.ye5s9.mongodb.net/CP-Dashboard?retryWrites=true&w=majority"
 const bcrypt = require('bcrypt');
 const { error } = require("console");
+const mongoose = require("mongoose");
 
-
+mongoose.connect(String(url),{ useNewUrlParser: true , useUnifiedTopology: true});
 app.use(bodyParser.json());
-app.get("/", function (req, res) {
+
+const userSchema = new mongoose.Schema(
+	{
+    cf_handle : {type : String, required:true, unique: true},
+	cf_email : {type : String,required : true},
+	inst_email : {type : String,required : true},
+	password : {type : String,required : true},
+	},
+	{ collection: 'Password-Details'}
+);
+const User = mongoose.model('UserSchema', userSchema);
+
+// app.get("/", function (req, res) {
     
-    res.render("index",{ message_login: ""});
-});
-app.get("/login", function (req, res) {
+//     res.render("index",{ message_login: ""});
+// });
+// app.get("/login", function (req, res) {
     
-    res.render("login",{ message_login: ""});
-});
+//     res.render("login",{ message_login: ""});
+// });
 
 app.post('/api/login', async (req, res) => {
 	const { cf_handle, password } = req.body;
-	var message = ""
-	MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("CP-Dashbaord");
-		var myobj = { _id:cf_handle, };
-		console.log(message);
-		dbo.collection("Password-Details").findOne(myobj, function(err, found_user) {
-			if (err)
-			{
-				message=err;
-			}
-			else
-			{
-				let fd = bcrypt.compareSync(password,found_user.password);
-				if(fd===true)
-					message="ok";
-				else
-				{
-					message="Password doesn't match";
-				}
-				db.close();
-			}
-			return res.json({ status: message });
-		});
-	});
+	const user = await User.findOne({ cf_handle }).lean()
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+	try {
+		if (await bcrypt.compare(password, user.password)) {
+
+			return res.json({ status: 'ok' })
+		}
+	} catch (error) {
+		return res.json({ status: 'error', error: error })
+	}
+
+	return res.json({ status: 'error', error: 'Invalid username/password' });
+
+
 })
 app.post('/api/register', async (req, res) => {
-	console.log(req.body)
+	// console.log(req.body)
 	const { cf_handle,cf_email,inst_email, password: plainTextPassword } = req.body
 
 	if (!cf_handle || typeof cf_handle !== 'string') {
@@ -69,41 +73,31 @@ app.post('/api/register', async (req, res) => {
 	}
 
 	/* Will be uncommented later */
-	if (plainTextPassword.length < 5) {
-		return res.json({
-			status: 'error',
-			error: 'Password too small. Should be atleast 6 characters'
-		})
-	}
+	// if (plainTextPassword.length < 5) {
+	// 	return res.json({
+	// 		status: 'error',
+	// 		error: 'Password too small. Should be atleast 6 characters'
+	// 	})
+	// }
 
 	const password = await bcrypt.hash(plainTextPassword, 10);
 	let message = "";
-	MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("CP-Dashbaord");
-		var myobj = { _id:cf_handle,cf_handle:cf_handle, cf_email:cf_email,inst_email:inst_email,password:password };
-		console.log(message);
-		dbo.collection("Password-Details").insertOne(myobj, function(err, new_res) {
-			if (err)
-			{
-				if(err.code===11000)
-				{
-					message = "User already exists";
-					console.log(message);
-				}
-				else
-				{
-					message=err;
-				}
-			}
-			else
-			{
-				message="ok";
-				db.close();
-			}
-			res.json({ status: message })
-		});
-	});
+	try {
+		const response = await User.create({
+			cf_handle,
+			cf_email,
+			inst_email,
+			password
+		})
+		console.log('User created successfully: ', response)
+		return res.json({ status : 'ok' });
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'CF Handle already in use' })
+		}
+		throw error
+	}
 })
 
 var port = process.env.PORT || 5000;
