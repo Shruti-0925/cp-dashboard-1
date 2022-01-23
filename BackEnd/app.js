ba42916aad595a6a7395d7f4cd93cedc7f973daa
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2022-01-23 18:06:58 +0530

    Improved Backend

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -11,29 +11,81 @@ const url="mongodb+srv://cp-user:12345@cluster.ye5s9.mongodb.net/CP-Dashboard?re
 const bcrypt = require('bcrypt');
 const { error } = require("console");
 const mongoose = require("mongoose");
-
+const axios = require('axios');
+const rp = require('request-promise')
 mongoose.connect(String(url),{ useNewUrlParser: true , useUnifiedTopology: true});
 app.use(bodyParser.json());
 
 const userSchema = new mongoose.Schema(
 	{
-    cf_handle : {type : String, required:true, unique: true},
-	cf_email : {type : String,required : true},
-	inst_email : {type : String,required : true},
-	password : {type : String,required : true},
+		cf_handle : {type : String, required:true, unique: true},
+		cf_email : {type : String,required : true},
+		inst_email : {type : String,required : true},
+		password : {type : String,required : true},
+		contests : {type : String,required : true},
+		max_rating : {type : String,required : true},
+		num_of_questions : {type : String,required : true},
 	},
 	{ collection: 'Password-Details'}
 );
 const User = mongoose.model('UserSchema', userSchema);
 
-// app.get("/", function (req, res) {
-    
-//     res.render("index",{ message_login: ""});
-// });
-// app.get("/login", function (req, res) {
-    
-//     res.render("login",{ message_login: ""});
-// });
+
+async function make_api_call(cf_handle) {
+	const cf_api="https://codeforces.com/api/";
+	const req1= await axios.get(cf_api + "user.status?handle=" + cf_handle); 
+	const req2= await axios.get(cf_api + "user.rating?handle=" + cf_handle); 
+	const req3= await axios.get(cf_api + "user.info?handles=" + cf_handle); 
+	let number_of_solved_questions=0;
+	let max_rating=0;
+	let number_of_contests=0;
+	async function get_details () {
+		try
+		{
+			let res = await axios.all([req1,req2, req3]).then(axios.spread((...responses) => {
+				const responseOne = responses[0];
+				const responseTwo = responses[1];
+				const responesThree = responses[2];
+				let all_attempted_questions = [];
+				for(let i=0;i<responseOne.data.result.length;i++)
+				{
+					if(responseOne.data.result[i].verdict === 'OK')
+					{
+						let str = String(responseOne.data.result[i].problem.contestId) + '-'+ responseOne.data.result[i].problem.index;
+						all_attempted_questions.push(str);
+					}
+				}
+				number_of_solved_questions = [...new Set(all_attempted_questions)].length;
+				number_of_contests = responseTwo.data.result.length;
+				max_rating = responesThree.data.result[0].maxRating;
+				// console.log(cf_handle);
+				// console.log("Number of Solved Questions : "+ number_of_solved_questions);
+				// console.log("Number of contests : " + responseTwo.data.result.length);
+				// console.log("Max Rating : " + responesThree.data.result[0].maxRating);
+			}))
+		}
+		catch(err){
+			console.log(err);
+		}
+	}
+	await get_details();
+	return {num_of_questions : number_of_solved_questions , num_of_contests : number_of_contests , max_rating : max_rating};
+}
+
+app.get("/login", async (req, res) => {
+	const cursor = User.find().cursor();
+	
+	for (let user = await cursor.next(); user != null; user = await cursor.next())
+	{
+		let result;
+		await make_api_call(user.cf_handle).then((response) => {
+			// console.log(response);
+			result=response;
+		});
+		console.log(result);
+	}
+    res.render("login");
+});
 
 app.post('/api/login', async (req, res) => {
 	const { cf_handle, password } = req.body;
@@ -81,13 +133,22 @@ app.post('/api/register', async (req, res) => {
 	// }
 
 	const password = await bcrypt.hash(plainTextPassword, 10);
-	let message = "";
+	let result;
+	await make_api_call(cf_handle).then((response) => {
+		result=response;
+	});
+	let num_of_questions = result.num_of_questions;
+	let max_rating = result.max_rating;
+	let contests = result.num_of_contests;
 	try {
 		const response = await User.create({
 			cf_handle,
 			cf_email,
 			inst_email,
-			password
+			password,
+			contests,
+			max_rating,
+			num_of_questions
 		})
 		console.log('User created successfully: ', response)
 		return res.json({ status : 'ok' });
@@ -99,6 +160,13 @@ app.post('/api/register', async (req, res) => {
 		throw error
 	}
 })
+
+
+app.get("/leaderboard", async (req, res) => {
+	const data = await User.find({}).lean();
+	res.render("leaderboard",{data : data});
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
