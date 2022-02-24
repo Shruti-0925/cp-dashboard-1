779b87e4df665aa6ae2368d1197bad739aa58a2c
pratiksha-dse <b20165@students.iitmandi.ac.in>
2022-02-25 01:46:43 +0530

    issue fixing

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -415,7 +415,7 @@ function Register() {
                     Submit OTP.
                 </button> : null}
                 <br />
-                <span>Already have an account? <Link className="lreg" to="/#login">Login</Link> </span>
+                <span>Already have an account? <Link className="lreg" to="#/login">Login</Link> </span>
             </form>
             <ul class="box-area">
                 <li></li>
